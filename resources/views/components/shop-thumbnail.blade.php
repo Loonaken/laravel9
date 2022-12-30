849b39908b2c71adef4c6fa574f79a7d95e0cd62
Loonaken <kendoku328@gmail.com>
2022-12-30 22:43:43 +0900

    Image

--- resources/views/components/shop-thumbnail.blade.php
+++ resources/views/components/shop-thumbnail.blade.php
@@ -2,6 +2,6 @@
   @if (empty($shop->filename))
   <img src="{{asset('images/no_image.jpg')}}" alt="">
   @else
-  <img src="{{asset('storage/shops/' . $shop->filename)}}"  alt="">
+  <img src="{{asset('storage/shops/' . $filename)}}"  alt="">
   @endif
 </div>
