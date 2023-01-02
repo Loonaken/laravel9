ab5689f6255411aa6e36c9e17877bbb2fe3d82c8
Loonaken <kendoku328@gmail.com>
2023-01-02 20:18:58 +0900

    image delete

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Image;
+use App\Models\Product;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
@@ -113,6 +114,34 @@ class ImageController extends Controller
     {
         {
             $image = Image::findOrFail($id);
+
+            $imageInProducts = Product::where('image1', $image->id)
+            ->orWhere('image2', $image->id)
+            ->orWhere('image3', $image->id)
+            ->orWhere('image4', $image->id)
+            ->get();
+
+            if($imageInProducts){
+                $imageInProducts->each(function($product) use($image){
+                    if($product->image1 === $image->id){
+                        $product->image1 = null;
+                        $product->save();
+                    }
+                    if($product->image2 === $image->id){
+                        $product->image2 = null;
+                        $product->save();
+                    }
+                    if($product->image3 === $image->id){
+                        $product->image3 = null;
+                        $product->save();
+                    }
+                    if($product->image4 === $image->id){
+                        $product->image4 = null;
+                        $product->save();
+                    }
+                });
+            }
+
             $filePath = 'public/products/' . $image->filename;
             // storageにある画像データを削除する必要があるため、パスを指定する。
             // 画像はImageであれば、Productsの配下にあるため、
