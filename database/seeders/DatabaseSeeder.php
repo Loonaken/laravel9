f9f51ac1dace9050163ab5a30a8bce7940520b9c
Loonaken <kendoku328@gmail.com>
2023-01-02 21:42:04 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,7 +25,11 @@ class DatabaseSeeder extends Seeder
             // ProductSeeder::class,
             // StockSeeder::class
             UserSeeder::class
+
         ]);
+
+            Product::factory(100)->create();
+            Stock::factory(100)->create();
 
         // \App\Models\User::factory(10)->create();
     }
