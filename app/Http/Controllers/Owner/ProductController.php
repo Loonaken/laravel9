ce2fd3b7375355750ea9a27223d24b99ba3123e7
Loonaken <kendoku328@gmail.com>
2023-01-02 16:50:01 +0900

    product edit

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -130,12 +130,6 @@ class ProductController extends Controller
         ->route('owner.products.index')
         ->with(['message'=> '商品登録が完了しました。' , 'status'=>'info']);
 
-
-
-
-
-
-
         // $product = Product::create([
         //     'name' => $request->name,
         //     'information'=>$request->information,
@@ -150,26 +144,26 @@ class ProductController extends Controller
         // ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->product_id)
+                ->sum('quantity');
+
+        $shops = Shop::where('owner_id', Auth::id())
+        ->select('id', 'name')
+        ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+        ->select('id','title','filename')
+        ->orderBy('updated_at', 'desc')
+        ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
+
+        return view ('owner.products.edit' , compact('product', 'quantity', 'shops', 'images', 'categories'));
     }
 
     /**
